7b278bb424373a48906b5da6bd18bee5cb64579d
Nathan <nathanmcneil@pm.me>
2020-07-14 14:19:43 -0700

    added search componenet

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input
+}
+  from 'reactstrap'
 
-const Search = () => {
+const Search = props => {
   return (
-    
+    <>
+      <Form inline>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Label htmlFor="user" className="mr-sm-2"></Label>
+          <Input
+            type="user"
+            name="user"
+            id="user"
+            placeholder="Enter Search Criteria..."
+            onChange={props.handleInputChange}
+            value={props.first_name}
+          />
+        </FormGroup>
+        <Button onClick={props.handleSubmit}>Search</Button>
+      </Form>
+    </>
   )
 }
 
