381d693b0b97c16f27f2f6e5d33adcf1aa6ad376
Nathan <nathanmcneil@pm.me>
2020-07-14 14:20:29 -0700

    added state and app.js functions

--- src/App.js
+++ src/App.js
@@ -1,32 +1,63 @@
-import React from 'react';
-import './App.css';
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  Button
-} from 'reactstrap'
+import React, { Component } from 'react'
+// import './App.css'
+import users from './Users.json'
+import Card from './components/Card'
+import Search from './components/Search'
+import UserTable from './components/UserTable'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    console.log(UserTable.users)
+  }
+  state = {
+    first_name: '',
+    users
+  }
+
+  // grab value by text input
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  // grab value from text input and create card
+  handleSubmit = event => {
+    event.preventDefault()
+    // console.log('ping')
+
+    this.setState({
+      users: {
+        id: this.state.id,
+        first_name: this.state.first_name,
+        last_name: this.state.last_name,
+        department: this.state.department,
+        job_title: this.state.job_title,
+        email: this.state.email,
+        phone_number: this.state.phone_number
+      }
+    })
+  }
+
+  // return JSX
+  render() {
+    return (
+      <>
+        <h1>Employee Directory</h1>
+        <Search
+          id={this.state.id}
+          first_name={this.state.first_name}
+          last_name={this.state.last}
+          department={this.state.department}
+          job_title={this.state.job_title}
+          email={this.state.email}
+          phone_number={this.state.phone_number}
+          handleInputChange={this.handleInputChange}
+          handleSubmit={this.handleSubmit} />
+        {this.state.first_name ? <Card userCard={this.state.first_name} /> : null}
+        <hr />
+        <UserTable />
+      </>
+    )
+  }
 }
 
 export default App;
